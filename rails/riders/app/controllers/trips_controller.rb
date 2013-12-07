cb7eefb7217edd048b3edca429a722169e92f07d
Ni Mu <nmu@onekingslane.com>
2013-12-06 21:35:34 -0800

    tripscontroller

---
+++ rails/riders/app/controllers/trips_controller.rb
@@ -0,0 +1,18 @@
+class TripsController < ApplicationController
+
+  def edit
+
+  end
+
+
+  # playback/data consumption: mapview 
+  def show
+
+  end
+
+  # add notes along gps locations during a trip
+  def record
+
+  end
+
+end
