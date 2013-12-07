68e36fe2397564d3fc14796658698695eefa9521
Tinou Bao <tbao@onekingslane.com>
2013-12-06 20:38:03 -0800

    rails riders app

---
+++ rails/riders/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riders::Application.config.secret_token = '32e5a3180632ecaf17ffea75b3049c3724c5cd35402d8c8a0b8fee5b1b57f91ec706712a1033057d0aaf674553160f72a98c257b2d184f9f67e6dff3ad3f4b26'
